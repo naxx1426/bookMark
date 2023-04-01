647a42014463837a8fbeb94b8fbea33fa9b77c78
naxx <1426384849@qq.com>
2023-04-01 11:39:50 +0800

    再次补充了一系列东西，然而还没有完全完善

--- bookMark_app/forms.py
+++ bookMark_app/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from phonenumber_field.formfields import PhoneNumberField
+from ckeditor_uploader.fields import RichTextUploadingFormField
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from .models import UseInfo
 
 
 class UserInfoForm(forms.ModelForm):
     mailbox = forms.EmailField(label='邮箱', required=True)
     phone_number = PhoneNumberField(label='手机号', required=True)
+    introduction = RichTextUploadingFormField(widget=CKEditorUploadingWidget, label='个人简介', required=True,
+                                              config_name="default")
 
     class Meta:
         model = UseInfo
